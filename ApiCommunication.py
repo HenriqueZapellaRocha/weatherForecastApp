9bc59d70014787b34c9b47c64eb91af1cf2e34d2
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2024-05-13 16:22:51 -0300

    its python but im typing the funcionts return

--- ApiCommunication.py
+++ ApiCommunication.py
@@ -1,7 +1,8 @@
+from requests import *
 import requests
 
 #verify cities registered in api on this token
-def verifyCitiesAddedInApi(token:str):
+def verifyCitiesAddedInApi(token:str) -> Response:
     url =  'http://apiadvisor.climatempo.com.br/api-manager/user-token/'+token+'/locales'
     r = requests.get(url)
     #verify if th response is a error, if it's return none, else returne the cities id registered in api
@@ -10,19 +11,21 @@ def verifyCitiesAddedInApi(token:str):
     else:
         return r.json()['locales']
     
-def nameCityByiD(token: str, cityId: int):
+def nameCityByiD(token: str, cityId: int) -> Response:
     url =  'http://apiadvisor.climatempo.com.br/api/v1/locale/city/3477?token='+token
     r = requests.get(url)
-    
+    #verify if th response is a error, if it's return none, else return the name of the city by id
     if r.status_code == 400:
         return None 
     else:
         return r.json()['name']
-    
-def currentWeather(token: str, cityId: int):
+
+#search the current wheater in api using city id
+def currentWeather(token: str, cityId: int) -> Response:
     url=  'http://apiadvisor.climatempo.com.br/api/v1/weather/locale/3477/current?token='+token
-    r = requests.get(url)
     
+     #verify if th response is a error, if it's return none, else return the current wheater data 
+    r = requests.get(url)
     if r.status_code == 400:
         return None
     else: 
